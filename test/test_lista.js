83079421d5ab64fc25f305ab09dd9eb230e7ca67
juan <juanfri_tentor@hotmail.com>
2020-11-06 19:19:45 -0300

    Commit inicial

---
+++ test/test_lista.js
@@ -0,0 +1,20 @@
+/**
+ * En una lista vacía hay cero elementos.
+ * Cuando se agrega un elemento a una lista vacía hay un elemento.
+ * En una lista vacía no se encuentra ninguna clave.
+ * Cuando se agrega un elemento a una lista vacía se puede recuperar el valor a partir de la clave.
+ * Cuando se agrega una clave que ya está en la lista no cambia la cantidad de elementos almacenados.
+ * Cuando se agregan un elemento a una lista vacía la lista de claves esta ordenada.
+ * Cuando se agrega un elemento al principio la lista de claves esta ordenada.
+ * Cuando se agrega un elemento al final la lista de claves esta ordenada.
+ * **/
+
+const assert = require('chai').assert;
+const Lista = require('../src/lista.js');
+
+describe('en una lista vacia', function () {
+	it('hay cero elementos', function () {
+		var lista = new Lista();
+		assert.equal(lista.count(), 0);
+	});
+});
