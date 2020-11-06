2e138b6b76821cda37bb509de058e12389df27bc
juan <juanfri_tentor@hotmail.com>
2020-11-06 20:03:09 -0300

    Primeros dos test

--- test/test_lista.js
+++ test/test_lista.js
@@ -12,9 +12,17 @@
 const assert = require('chai').assert;
 const Lista = require('../src/lista.js');
 
-describe('en una lista vacia', function () {
-	it('hay cero elementos', function () {
+describe('En una lista vacia', function () {
+	it('Hay cero elementos', function () {
 		var lista = new Lista();
 		assert.equal(lista.count(), 0);
+	});
+});
+
+describe('Cuando se agrega un elemento en una lista vacia', function () {
+	it('Hay un elemento', function () {
+		var lista = new Lista();
+		lista.add('name', 'juan');
+		assert.equal(lista.count(), 1);
 	});
 });
